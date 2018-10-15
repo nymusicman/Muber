86dc9b05ac64ae681087e614508d21857309d2de
John O'Keefe <jokeefe@fastmail.com>
2018-10-15 16:00:52 -0500

    10-15 stop

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,6 @@ const DriversController = require('../controller/drivers_controller');
 
 module.exports = (app) => {
     app.get('/api', DriversController.greeting);
+
+    app.post('/api/drivers', DriversController.create);
 }
